834c328870cd4b5cfb4dffa78bb9bc9718673678
luije87 <luije87@gmail.com>
2024-09-12 16:41:19 -0400

    chore: Update OpenAI API key in route.ts

--- app/api/rewrite/route.ts
+++ app/api/rewrite/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: NextRequest) {
   const bio = json.bio;
 
   const openai = new OpenAI({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
   });
 
