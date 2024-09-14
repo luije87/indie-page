0b971de570bb3a532393c7854ce013510ca380e9
luije87 <luije87@gmail.com>
2024-09-14 09:54:40 -0400

    chore: Update avatar selection UI and page layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,53 @@
 import "@/app/globals.css";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Hi! Welcome to Indie Page",
-  description: "The fastest way to ship your projects",
+  title: "Welcome to Indie Page",
+  description: "The fastest way to showcase your projects.",
+  keywords: [
+    "indie page",
+    "build webpage",
+    "project shipping",
+    "portfolio",
+    "website builder",
+    "customize webpage",
+  ],
+  authors: [{ name: "Luije", url: "https://indie-page-ashy.vercel.app/luije" }],
+  themeColor: "#ff6600",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon-32x32.png",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  openGraph: {
+    type: "website",
+    url: "https://indie-page-ashy.vercel.app",
+    title: "Welcome to Indie Page",
+    description: "The fastest way to showcase your projects.",
+    siteName: "Indie Page",
+    images: [
+      {
+        url: "https://indie-page-ashy.vercel.app/android-chrome-512x512.png",
+        width: 800,
+        height: 600,
+        alt: "Indie Page logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@luije87",
+    title: "Welcome to Indie Page",
+    description: "The fastest way to showcase your projects.",
+    images: ["https://yourwebsite.com/images/twitter-image.png"],
+  },
+  alternates: {
+    canonical: "https://indie-page-ashy.vercel.app",
+  },
 };
 
 export default async function RootLayout({
