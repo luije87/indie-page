42c7b09e3350f67ca0e7716c813ea35cc13bf525
luije87 <luije87@gmail.com>
2024-10-09 17:58:48 -0400

    chore: Update ViewLink component to use Next.js Link
    - Replace the anchor tag with Next.js Link component in ViewLink.tsx
    - This improves the performance and accessibility of the link

--- components/ui/view-link.tsx
+++ components/ui/view-link.tsx
@@ -1,18 +1,15 @@
 "use client";
 import { UserContext } from "@/app/context/provider";
 import { ArrowUpIcon, ArrowUpRightIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
 import { useContext } from "react";
 
 export default function ViewLink() {
   const { user } = useContext(UserContext);
   return (
-    <a
-      className="hover:link flex items-center"
-      href={`/${user.slug}`}
-      target="blank"
-    >
+    <Link className="hover:link flex items-center" href={`/${user.slug}`}>
       view
       <ArrowUpRightIcon className="w-4 h-4 ml-2" />
-    </a>
+    </Link>
   );
 }
